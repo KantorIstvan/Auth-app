15e648de4042d43810163481eaf0a3a979dd6b05
Kántor István <kantoristvan17@gmail.com>
2025-04-04 13:35:21 +0200

    fix2

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,16 @@
-export default function UserProfile({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default function UserProfile({ params }: Props) {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
       <h1>Profile</h1>
       <hr />
       <p className="text-4xl">
         Profile page
-        <span className="p-2 rounded bg-orange-500 text-black">
-          {params.id}
-        </span>
+        <span className="p-2 rounded bg-green-500 text-black">{params.id}</span>
       </p>
     </div>
   );
